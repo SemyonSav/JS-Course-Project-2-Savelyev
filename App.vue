c4dd82131c43fd83a1313b86a3b5c44ef707bc50
Semen Savelev <ctvbxm@mail.ru>
2022-12-18 03:00:34 +0300

    Init project

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="test">
+        {{ text }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {
+            text: 'Хуй ца сосни вебпак'
+        }
+    }
+}
+</script>
+
+<style>
+.test {
+    color: red;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const {VueLoaderPlugin} = require("vue-loader");
+
+
+module.exports = {
+    entry: path.join(__dirname, 'index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/dist/',
+        filename: 'build.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+};
