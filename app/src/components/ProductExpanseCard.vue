437dcf91ebfc1d6232072973862a5dd26365f463
Semen Savelev <ctvbxm@mail.ru>
2022-12-25 21:32:52 +0300

    Add product card

---
+++ app/src/components/ProductExpanseCard.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "ProductExpanseCard"
+}
+</script>
+
+<style scoped>
+
+</style>
--- app/src/pages/ProductsPage.vue
+++ app/src/pages/ProductsPage.vue
@@ -1,19 +1,45 @@
 <template>
     <div>
         <Header></Header>
-        Products
+        <div class="main">
+            <input
+                type="text"
+                class="product-find-input"
+            >
+            <div class="products-cards">
+                <ProductCard
+                    class="products-cards__card"
+                    v-for="card in 6"
+                >
+                </ProductCard>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import Header from '../components/Header';
+import ProductCard from "../components/ProductCard";
 
 export default {
     name: 'ProductsPage',
-    components: { Header },
+    components: {ProductCard, Header },
 };
 </script>
 
 <style scoped>
+    .main {
+        width: 80%;
+        margin: 0 10%;
+    }
 
+    .products-cards__card {
+        width: 30%;
+        margin: 10px 10px;
+    }
+
+    .products-cards {
+        display: flex;
+        flex-wrap: wrap;
+    }
 </style>
